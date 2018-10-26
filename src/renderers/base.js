787e48ef4564e94a99a7be03756ed2b718f32ffe
asacoolguy <asacoolguy@gmail.com>
2018-10-26 08:50:55 -0400

    did some pseudo code

--- src/renderers/base.js
+++ src/renderers/base.js
@@ -15,12 +15,31 @@ export default class BaseRenderer {
     // TODO: Update the cluster texture with the count and indices of the lights in each cluster
     // This will take some time. The math is nontrivial...
 
+    let index = 0;
+    // loop through each light to see what cluster it contains
+    for (let i = 0; i < NUM_LIGHTS; ++i) {
+      // first translate the light to NDC screen space 
+
+      // then find this light's AABB 
+      
+      // see what clusters this AABB overlaps
+
+    }
+
+    // TODO: need to figure out better way to do this. maybe loop through lights -> xyz instead of xyz-> lights
+    
     for (let z = 0; z < this._zSlices; ++z) {
       for (let y = 0; y < this._ySlices; ++y) {
         for (let x = 0; x < this._xSlices; ++x) {
           let i = x + y * this._xSlices + z * this._xSlices * this._ySlices;
           // Reset the light count to 0 for every cluster
           this._clusterTexture.buffer[this._clusterTexture.bufferIndex(i, 0)] = 0;
+
+          // loop through each light and check if it is
+          for (let i = 0; i < NUM_LIGHTS; ++i) {
+            // if light is in, pack it in the clusterTexture
+            this._lightTexture.buffer[this._lightTexture.bufferIndex(i, 0) + 0] = scene.lights[i].position[0];
+          }
         }
       }
     }
